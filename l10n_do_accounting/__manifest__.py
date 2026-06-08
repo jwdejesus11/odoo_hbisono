0053ea6496874251e5a2150cd4cd3438769b903c
dejesus <support@itz.org>
2026-06-08 13:31:30 -0400

    Report: Filter out Proveedor, Marca, and Categoria from printed Quotation and Invoice descriptions

--- l10n_do_accounting/__manifest__.py
+++ l10n_do_accounting/__manifest__.py
@@ -11,7 +11,7 @@
     "version": "17.0.1.0.4",
     "countries": ["do"],
     # any module necessary for this one to work correctly
-    "depends": ["l10n_latam_invoice_document", "l10n_do"],
+    "depends": ["l10n_latam_invoice_document", "l10n_do", "sale"],
     # always loaded
     "data": [
         "security/ir.model.access.csv",
